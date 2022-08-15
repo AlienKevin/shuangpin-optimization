c724dfe33f5d5f3e4454da5a7999139194d77010
Kevin Li <kevinli020508@gmail.com>
2022-08-15 11:14:01 -0400

    Add ziranma config

--- shuangpin.py
+++ shuangpin.py
@@ -302,6 +302,66 @@ foxi_config = ShuangpinConfig(
 )
 
 
+### Ziranma (自然码) configurations
+ziranma_config = ShuangpinConfig(
+    final_layout={
+        "iu": "q",
+        "ua": "w",
+        "uan": "r",
+        "ue": "t",
+        "uai": "y",
+        "uo": "o",
+        "un": "p",
+        "ong": "s",
+        "uang": "d",
+        "en": "f",
+        "eng": "g",
+        "ang": "h",
+        "an": "j",
+        "ao": "k",
+        "ai": "l",
+        "ei": "z",
+        "ie": "x",
+        "iao": "c",
+        "ui": "v",
+        "ou": "b",
+        "in": "n",
+        "ian": "m",
+    },
+    # keys must be in the order of the digraph_initials list
+    digraph_initial_layout={
+        "zh": "v",
+        "ch": "i",
+        "sh": "u",
+    },
+    zero_consonant_layout={
+        "a": ("a", "a"),
+        "e": ("e", "e"),
+        "o": ("o", "o"),
+        "ai": ("a", "i"),
+        "ei": ("e", "i"),
+        "ou": ("o", "u"),
+        "an": ("a", "n"),
+        "en": ("e", "n"),
+        "ang": ("a", "h"),
+        "eng": ("e", "g"),
+        "ao": ("a", "o"),
+        "er": ("e", "r"),
+    },
+    # standards appear on the top of the key in the keyboard diagram
+    # variants appear below the standards
+    variant_to_standard_finals={
+        "ve": "ue",
+        "o": "uo",
+        "iong": "ong",
+        "ing": "uai",
+        "iang": "uang",
+        "ia": "ua",
+        "v": "ui",
+    },
+)
+
+
 def get_random_final_layout(
     final_layout: dict[str, str], variant_to_standard_finals: dict[str, str]
 ) -> dict[str, str]:
@@ -481,4 +541,5 @@ def get_score(
 
 
 print("xiaohe score = {}".format(get_score(xiaohe_config)))
+print("ziranma score = {}".format(get_score(ziranma_config)))
 print("foxi score = {}".format(get_score(foxi_config)))
